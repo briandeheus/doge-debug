f587adc47a03bc144fcd66e3668f864e9908a8ff
Ionică Bizău <bizauionica@yahoo.com>
2014-06-15 17:50:32 +0300

    Some messages

--- test/index.js
+++ test/index.js
@@ -4,6 +4,6 @@ var i = 0;
 setInterval(function () {
     console.log(" info - " + new Date() + " - Dummy message");
     if (++i === 8) {
-        Doge();
+        Doge("Look at this: ", "Hello World!");
     }
 }, 400);
